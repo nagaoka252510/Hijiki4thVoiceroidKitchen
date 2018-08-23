bc27de5d4f9666da9be2a1d3f968911658109ac5
ih6109 <nagaoka252510@gmail.com>
2018-08-23 21:46:58 +0900

    developにpush

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 # -*- coding: utf-8 -*-
-import requests
 import json
 import urllib.parse as urlparse
+import urllib
+import requests
 
 NICO_CONTENTS_SEARCH_API_END_POINT = "http://api.search.nicovideo.jp/api/v2/video/contents/search"
 
-# TODO APIを叩くURLの生成は別クラスにまとめたい
-search_url = NICO_CONTENTS_SEARCH_API_END_POINT
-search_url += "?q=" + urlparse.quote("VOICEROIDキッチン 第四回ひじき祭")
-search_url += "&targets=tagsExact"
-search_url += "&_limit=100"
-search_url += "&_sort=-startTime"
-search_url += "&_context=apitest"
-search_url += "&fields=contentId,title,description,tags,categoryTags,viewCounter,mylistCounter,commentCounter,startTime,lastCommentTime,lengthSeconds"
+def search_voiro_kitchen_hijiki():
+    # TODO APIを叩くURLの生成は別クラスにまとめたい
+    search_url = NICO_CONTENTS_SEARCH_API_END_POINT
+    search_url += "?q=" + urlparse.quote("VOICEROIDキッチン 第四回ひじき祭")
+    search_url += "&targets=tagsExact"
+    search_url += "&_limit=100"
+    search_url += "&_sort=-startTime"
+    search_url += "&_context=apitest"
+    search_url += "&fields=contentId,title,description,tags,categoryTags,viewCounter,mylistCounter,commentCounter,startTime,lastCommentTime,lengthSeconds"
 
-#print(search_url)
+    #print(search_url)
 
-r = requests.get(search_url)
-req_json = json.dumps(r.json(),ensure_ascii=False)
+    r = requests.get(search_url)
+    req_json = json.dumps(r.json(),ensure_ascii=False)
+    req_json_dict = json.loads(req_json)
 
-# TODO sqlite3に情報を保存する
-with open("./voiceroid_kitchen.txt","w",encoding="utf_8") as wf:
-    wf.write(req_json)
+    return req_json_dict
 
-#print(r)
+def insert_json_data(json_dict):
+    for data in json_dict["data"]:
+        print(data["contentId"])
 
-#print(req_json)
+if __name__ == "__main__":
+    data = search_voiro_kitchen_hijiki()
+    insert_json_data(data)
